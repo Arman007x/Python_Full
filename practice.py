9f7c4fb8acf22f1c23996e590e55f4ee87e29bc4
Arman Chowdhury <58934298+Arman007x@users.noreply.github.com>
2026-05-24 13:02:20 +0600

    Practiced file

---
+++ practice.py
@@ -0,0 +1,57 @@
+# # ###### Output -------------------
+
+# print("Hello world")
+# print("Its me Arman")
+# print("My name is Arman , I am 23 y o")
+# print(21)
+# print(21*22)
+# print(1+1-1/1)
+
+# # print() is a function. its use for output.
+
+
+
+# # # # # # Variable and value. a = 1 , a is a variable, 1 is Value, = is assignment operator, in python 1 is stored in a variable, whenever we call a, It will show 1 aas a output.
+
+# name = "Arman" # String
+# age = 23
+# gpa = 3.31
+# age2 = age
+# age3 = age+1
+# age4 = age*age
+
+# print("My name is : ",name)
+# print("My age is : ",age)
+# print("My cgpa is : ",gpa)
+# print(age)
+# print(age2)
+# print(age3)
+# print(age4)
+
+# # What Type of value it is ?, Data types.
+
+# print(type(name))
+# print(type(age))
+# print(type(gpa))
+
+
+# Data types, // 1.Integers(1,20,100) // 2.String("Something",'something') // 3.Float(3.14,3.31,40.1) // 4.Boolean(True,False) // 5.None
+
+# Keywords. ==> those are the reserved words in python, Types of keywords ==>
+
+# Values: True, False, None.
+# Operators: and, or, not, in, is
+# Control Flow: if, elif, else, for, while, break, continue, pass. 
+# Structure: def, class, with, as, lambda, yield, return. 
+# Exceptions: try, except, finally, raise, assert. 
+# Imports: import, from, as. 
+# Scope: global, nonlocal, del. 
+# Async: async, await
+
+ 
+# print sum
+
+a = 2999
+b = 50
+sum = a+b
+print(sum)
